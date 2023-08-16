f1e576d5311c341ce464399e771b49fac131e070
chrispin ciza <104005961+chrispinciza01@users.noreply.github.com>
2023-08-16 14:39:49 +0300

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
-# Enable the user holberton to login and open files without error.
+# Enable the user to login and open files without error.
 
-# Increase hard file limit for Holberton user.
+# Increase hard file limit for holberton user.
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
